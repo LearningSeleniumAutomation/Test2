e021e6e32d3cdcc1212aa8bea24db7a23b2ee973
Himanshu Kumar <himanshu.kumar@microfocus.com>
2019-08-22 10:25:35 +0530

    Commiting Hello world prog

---
+++ src/com/moudule1/java/qa/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.moudule1.java.qa;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(">>Hello Java!<<");
+	}
+
+}
